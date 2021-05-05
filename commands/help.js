160eb2a663306aa88e16fdf2afa28f3b3d7aa292
SJTechy <SJTechy@users.noreply.github.com>
2021-05-05 13:35:49 -0700

    Help changes

--- commands/help.js
+++ commands/help.js
@@ -5,14 +5,16 @@ require("dotenv").config();
 function help(msg) {
     const embed = new Discord.MessageEmbed()
     .setTitle("Quark Help")
-    .setDescription("Science for Discord")
-    .addFields(
-      {value: "The NASA Picture of the Day", name: "`q?apod`", inline: false},
-      {value: "An image from NASA's Curiosity rover!", name: "`q?marsimg`", inline: false},
-      {value: "Instructions to add Quark for your server", name: "`q?getquark`", inline: false},
-      {value: "Search for an image in NASA's database.", name: "`q?nasaimg 'search-param'`", inline: false},
-      {value: "https://github.com/SJTechy/quark", name: "`Github`", inline: false}
-    )
+    .setDescription(`
+    **NASA Resources**
+    \`q?apod\` - The NASA Picture of the Day
+    \`q?marsimg\` - An image from NASA's Curiosity rover
+    \`q?getquark\` - Instructions to add Quark to your server
+
+    **Github**
+    \`Quark Repository\` - https://github.com/SJTechy/quark - Please star!
+
+    `)
     .setColor("#007ea8")
 
   msg.channel.send(embed)
