af44efc0a118036c19253d0cd33676bbe99f6d24
SJTechy <SJTechy@users.noreply.github.com>
2021-05-11 09:22:44 -0700

    Added Fallback for marsimg

--- commands/marsimg.js
+++ commands/marsimg.js
@@ -5,34 +5,31 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 
 function marsimg(msg) {
-    var randNum = Math.floor(
-        Math.random() * 100
-      );
-      var randNum2 = Math.floor(
-        Math.random() * 3100 + 1
-      );
+  var randNum = Math.floor(Math.random() * 100);
+  var randNum2 = Math.floor(Math.random() * 3100 + 1);
 
-    fetch(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randNum2}&api_key=${process.env.NASA_API_KEY}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
+  fetch(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randNum2}&api_key=${process.env.NASA_API_KEY}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const embed = new Discord.MessageEmbed()
+        .setTitle("Here is an image from the Curiosity rover!")
+        .setDescription(data.photos[randNum].earth_date)
+        .setImage(data.photos[randNum].img_src)
+        .setColor("#007ea8");
 
-          const embed = new Discord.MessageEmbed()
-            .setTitle("Here is an image from the Curiosity rover!")
-            .setDescription(data.photos[randNum].earth_date)
-            .setImage(data.photos[randNum].img_src)
-            .setColor("#007ea8")
+      msg.channel.send(embed);
+    })
+    .catch((err) => {
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Mars Image`)
+        .setDescription(`Sorry, an error occured. Please try again.`)
+        .setImage("https://media0.giphy.com/media/jpPZo8ScZenZ7yQK3v/giphy.gif")
+        .setColor("#007ea8");
 
-          msg.channel.send(embed)
-          // msg.channel.send(`Here is an image from the Curiosity rover! Date: ${data.photos[randNum].earth_date}`, {
-          //   files: [data.photos[randNum].img_src],
-          // });
-        })
-        .catch(err => {
-          msg.channel.send("Sorry, an error occurred.")
-          console.log(err)
-        })
+      msg.channel.send(embed);
+    });
 }
 
-module.exports = { marsimg }
+module.exports = { marsimg };
