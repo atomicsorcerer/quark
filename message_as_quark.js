278207497ea0b81f9eb579bbda718d1710b4b7db
SJTechy <67711756+atomicsorcerer@users.noreply.github.com>
2022-07-28 18:12:43 -0700

    message_as_quark can now search through all server owners.

--- message_as_quark.js
+++ message_as_quark.js
@@ -7,9 +7,9 @@ const client = new Client({ intents: ["Guilds"] });
 client.on("ready", async () => {
   console.log("Message as Quark Ready");
 
-  await client.users
-    .fetch("717907003120549908")
-    .then((user) => user.send("Hello, this is Quark"));
+  client.guilds.cache.map((guild) => {
+    client.users.fetch(guild.ownerId).then((user) => console.log(user.id));
+  });
 });
 
 client.login(process.env.ID);
